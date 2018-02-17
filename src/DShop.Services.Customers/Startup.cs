57ea1a174db9e767e615bb357d59d909e2fc1548
Piotr Gankiewicz <spetz90@gmail.com>
2018-02-17 22:46:16 +0100

    Service framework

---
+++ src/DShop.Services.Customers/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using DShop.Common.Bus.RabbitMq;
+using DShop.Common.Mvc;
+using DShop.Messages.Events.Identity;
+using DShop.Messages.Events.Products;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DShop.Services.Customers
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public IContainer Container { get; private set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddDefaultJsonOptions();
+            var builder = new ContainerBuilder();
+            builder.RegisterAssemblyTypes(Assembly.GetEntryAssembly())
+                    .AsImplementedInterfaces();
+            builder.Populate(services);
+            builder.AddRabbitMq();
+            //builder.AddMongoDB();
+            Container = builder.Build();
+
+            return new AutofacServiceProvider(Container);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, 
+            IApplicationLifetime applicationLifetime)
+        {
+            if (env.IsDevelopment() || env.EnvironmentName == "local")
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+            app.UseRabbitMq()
+                .SubscribeEvent<SignedUp>();
+
+            applicationLifetime.ApplicationStopped.Register(() => Container.Dispose());
+        }
+    }
+}
