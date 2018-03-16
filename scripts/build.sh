9908f5e0dd0ecb08c295fb13a3acd0995718a9cb
Piotr Gankiewicz <spetz90@gmail.com>
2018-03-16 10:39:22 +0100

    Fixed build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,5 +6,4 @@ case "$TRAVIS_BRANCH" in
     ;;
 esac
 
-dotnet restore -s "https://api.nuget.org/v3/index.json" -s "https://www.myget.org/F/dnc-dshop$MYGET_ENV/api/v3/index.json" --no-cache
-dotnet build -c Release --no-restore
+dotnet build -c Release --source "https://api.nuget.org/v3/index.json" --source "https://www.myget.org/F/dnc-dshop$MYGET_ENV/api/v3/index.json" --no-cache
