0b672c635729c9be0a6b919500592f550d4c1d3d
Piotr Gankiewicz <spetz90@gmail.com>
2018-03-16 10:34:25 +0100

    Final build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-dotnet build
+MYGET_ENV=""
+case "$TRAVIS_BRANCH" in
+  "develop")
+    MYGET_ENV="-dev"
+    ;;
+esac
+
+dotnet restore -s "https://api.nuget.org/v3/index.json" -s "https://www.myget.org/F/dnc-dshop$MYGET_ENV/api/v3/index.json" --no-cache
+dotnet build -c Release --no-restore
