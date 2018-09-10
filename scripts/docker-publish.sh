289fb7cffbf5f9b1070129082924c27836687bdb
Piotr Gankiewicz <spetz90@gmail.com>
2018-09-10 09:48:11 +0200

    Updated scripts and readme

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-DOCKER_ENV=''
 DOCKER_TAG=''
 
 case "$TRAVIS_BRANCH" in
   "master")
-    DOCKER_ENV=production
     DOCKER_TAG=latest
     ;;
   "develop")
-    DOCKER_ENV=development
     DOCKER_TAG=dev
     ;;    
 esac
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker build -f ./src/DShop.Services.Customers/Dockerfile.$DOCKER_ENV -t dshop.services.customers:$DOCKER_TAG ./src/DShop.Services.Customers
+docker build -t dshop.services.customers:$DOCKER_TAG .
 docker tag dshop.services.customers:$DOCKER_TAG $DOCKER_USERNAME/dshop.services.customers:$DOCKER_TAG
 docker push $DOCKER_USERNAME/dshop.services.customers:$DOCKER_TAG
